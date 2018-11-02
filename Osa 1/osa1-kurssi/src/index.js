4c71648de7dd24a3a1d89d1a65521b698e3d7e5d
Exeiya <krista.lindholm@gmail.com>
2018-11-02 18:23:27 +0200

    week 1 exercises 1.1-1.5

---
+++ Osa 1/osa1-kurssi/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => (
+  <h1>{props.kurssi.nimi}</h1>
+)
+
+const Sisalto = (props) => {
+  return (
+    <div>
+      <Osa osa={props.kurssi.osat[0]} />
+      <Osa osa={props.kurssi.osat[1]} />
+      <Osa osa={props.kurssi.osat[2]} />
+    </div>
+  )
+}
+
+const Osa = (props) => (
+  <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+)
+
+const Yhteensa = (props) => {
+  const tehtavia = props.kurssi.osat.reduce((sum, osa) => (sum + osa.tehtavia), 0)
+  return (
+    <p>yhteensä {tehtavia} tehtävää</p>
+  )
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
