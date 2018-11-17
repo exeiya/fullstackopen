b11be3ba86df003cbebb4f88daf9f49921089ae5
Exeiya <krista.lindholm@gmail.com>
2018-11-17 18:17:35 +0200

    week 4 exercise 4.2

--- Osa 4/bloglist-backend/index.js
+++ Osa 4/bloglist-backend/index.js
@@ -1,43 +1,27 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-module.exports = Blog
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to database')
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
