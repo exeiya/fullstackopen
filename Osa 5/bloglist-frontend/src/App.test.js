4b0a309a1b22c98123401de17fa3943d01f63fcc
Exeiya <krista.lindholm@gmail.com>
2018-11-27 01:57:30 +0200

    week 5 exercises 5.15 & 5.16

---
+++ Osa 5/bloglist-frontend/src/App.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import App from './App'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+
+import blogService from './services/blogs'
+
+
+describe('<App />', () => {
+  let app
+
+  describe('when user is not logged in', () => {
+    beforeEach(() => {
+      app = mount(<App />)
+    })
+  
+    it('renders only login form', () => {
+      app.update()
+      const blogComponents = app.find(Blog)
+      const loginFormComponent = app.find(LoginForm)
+      expect(blogComponents.length).toEqual(0)
+      expect(loginFormComponent.length).toEqual(1)
+    })
+  })
+
+  describe('when user is logged in', () => {
+    beforeEach(() => {
+      const user = {
+        username: 'tester',
+        token: '1231231214',
+        name: 'Teuvo Testaaja'
+      }
+      localStorage.setItem('loggedUser', JSON.stringify(user))
+
+      app = mount(<App />)
+    })
+  
+    it('all blogs are rendered', () => {
+      app.update()
+      const blogComponents = app.find(Blog)
+      const loginFormComponent = app.find(LoginForm)
+      expect(blogComponents.length).toEqual(blogService.blogs.length)
+      expect(loginFormComponent.length).toEqual(0)
+      
+    })
+  })
+  
+})
--- Osa 5/bloglist-frontend/src/components/Blog.test.js
+++ Osa 5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
